7fa270f9ee813ac357f6a722c5b9451074fd8df6
Nelson Chaves <nelsonchavespro@gmail.com>
2021-11-29 17:45:31 -0800

    add sign in form

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
     resources :reviews
   end
 
+  resource :session, only: [:new, :create, :destroy]
+
   resources :users
   get "signup" => "users#new"
-
+  get "signin" => "sessions#new"
+  
 end
